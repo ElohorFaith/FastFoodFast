7e169abb9de75a2293bdbddd59254c25ae06b912
ElohorFaith <pricelesselohor@gmail.com>
2018-09-08 16:26:58 +0100

    ft(getOneOrder): get one order

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import getOrders from '../controller/orderController';
-
+import orderController from '../controller/orderController';
 
 const router = express.Router();
 
-//get order request
-router.get('/orders', getOrders);
+// get order request
+router.get('/orders', orderController.getOrders);
+
+// get one order
+router.get('/orders/:id', orderController.getOneOrder);
 
 
 export default router;
