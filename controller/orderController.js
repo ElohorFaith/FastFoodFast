72df5bdfcf4f2aec507ef57ca4c2f45c23548db5
ElohorFaith <pricelesselohor@gmail.com>
2018-09-08 18:45:52 +0100

    ft(post-request): create an order

--- controller/orderController.js
+++ controller/orderController.js
@@ -3,15 +3,41 @@ import orders from '../models/orders';
 // get all orders
 const getOrders = (req, res) => res.send({
   orders,
+  message: 'fetch all orders request is successful',
+
 });
 
 // get one order
 const getOneOrder = (req, res) => {
   const singleOrder = orders.find(order => order.id === parseInt(req.params.id, 10));
-  if (!singleOrder) res.status(404).send('The order with the given Id is not found.');
-  res.send(singleOrder);
+  if (!singleOrder) {
+    res.status(404).send({
+      error: 'The order with the given Id is not found.',
+    });
+  }
+  res.send({
+    order: singleOrder,
+    message: 'fetch an order request is successful',
+  });
 };
+
+// create order
+const createOrder = (req, res) => {
+  const order = {
+    id: orders.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+    status: req.body.status,
+    date: req.body.date,
+  };
+  return res.send({
+    newOrder: order,
+    message: 'Order created successfully',
+  });
+};
+
 export default {
   getOrders,
   getOneOrder,
+  createOrder,
 };
